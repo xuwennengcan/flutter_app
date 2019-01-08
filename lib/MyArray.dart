eefee85aaae2f7b94c606811c878318782b0019a
xuwenneng <xuwenneng@huazhen.com>
2019-01-08 18:16:58 +0800

    学习Flutter基础组件

--- lib/MyArray.dart
+++ lib/MyArray.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/utils/Toast.dart';
 
 class MyArray extends StatelessWidget {
 
-  _pressButton(String string) {
+  _pressButton(context,String string) {
     print(string);
+    Toast().addToast(context,string);
   }
 
   @override
@@ -18,11 +20,11 @@ class MyArray extends StatelessWidget {
                   new Column(
                     children: <Widget>[
                       new RaisedButton(
-                        onPressed: (){_pressButton("点击绿色按钮");}, color: Colors.green,child: new Text("绿色按钮"),padding: EdgeInsets.all(10.0),),
+                        onPressed: (){_pressButton(context,"点击绿色按钮");}, color: Colors.green,child: new Text("绿色按钮"),padding: EdgeInsets.all(10.0),),
                       new RaisedButton(
-                        onPressed: (){_pressButton("点击红色按钮");}, color: Colors.red,child: new Text("红色按钮"),padding: EdgeInsets.all(10.0),),
+                        onPressed: (){_pressButton(context,"点击红色按钮");}, color: Colors.red,child: new Text("红色按钮"),padding: EdgeInsets.all(10.0),),
                       new RaisedButton(
-                        onPressed: (){_pressButton("点击黄色按钮");}, color: Colors.yellow,child: new Text("黄色按钮"),padding: EdgeInsets.all(10.0),)
+                        onPressed: (){_pressButton(context,"点击黄色按钮");}, color: Colors.yellow,child: new Text("黄色按钮"),padding: EdgeInsets.all(10.0),)
                     ],
                   )
                 ],
@@ -34,11 +36,11 @@ class MyArray extends StatelessWidget {
                   new Row(
                     children: <Widget>[
                       new RaisedButton(
-                        onPressed: (){_pressButton("点击绿色按钮");}, color: Colors.green,child: new Text("绿色按钮"),padding: EdgeInsets.all(10.0),),
+                        onPressed: (){_pressButton(context,"点击绿色按钮");}, color: Colors.green,child: new Text("绿色按钮"),padding: EdgeInsets.all(10.0),),
                       new RaisedButton(
-                        onPressed: (){_pressButton("点击红色按钮");}, color: Colors.red,child: new Text("红色按钮"),padding: EdgeInsets.all(10.0),),
+                        onPressed: (){_pressButton(context,"点击红色按钮");}, color: Colors.red,child: new Text("红色按钮"),padding: EdgeInsets.all(10.0),),
                       new RaisedButton(
-                        onPressed: (){_pressButton("点击黄色按钮");}, color: Colors.yellow,child: new Text("黄色按钮"),padding: EdgeInsets.all(10.0),)
+                        onPressed: (){_pressButton(context,"点击黄色按钮");}, color: Colors.yellow,child: new Text("黄色按钮"),padding: EdgeInsets.all(10.0),)
                     ],
                   )
                 ],
--- lib/utils/Toast.dart
+++ lib/utils/Toast.dart
@@ -1,3 +1,24 @@
+import 'package:flutter/material.dart';
+
 class Toast{
-  
+
+  OverlayState overlayState;
+  OverlayEntry overlayEntry;
+
+  OverlayState getToast(context,String string){
+    if(overlayState==null)
+      overlayState = Overlay.of(context);
+    if(overlayEntry==null)
+      overlayEntry = OverlayEntry(
+        builder: (BuildContext context) => Positioned(
+          child: Text(string),
+        ));
+    //移除
+    overlayEntry.remove();
+    return overlayState;
+  }
+
+  void addToast(BuildContext context,String string){
+    getToast(context,string).insert(overlayEntry);
+  }
 }
