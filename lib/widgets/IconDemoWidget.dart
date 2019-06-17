b6ce08bfb7bd554b0527cef85882e67b8ba5c4d9
can <13611414180@163.com>
2019-06-17 13:37:01 +0800

    Flutter页面

--- lib/widgets/IconDemoWidget.dart
+++ lib/widgets/IconDemoWidget.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 
+///这是一个图文的icon按钮
 class IconDemoWidget extends StatelessWidget {
+  ///图标
   final IconData iconData;
+
+  ///文字
   final String text;
 
   const IconDemoWidget({Key key, this.iconData, this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    ///Expanded均分Row、Column
     return new Expanded(
+
+        ///水平方向布局
         child: new Row(
       textDirection: TextDirection.ltr,
       mainAxisAlignment: MainAxisAlignment.center,
@@ -16,6 +23,8 @@ class IconDemoWidget extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
         newIcon(iconData),
+
+        ///设置间距
         new Padding(padding: EdgeInsets.all(5)),
         newText(text)
       ],
@@ -23,11 +32,13 @@ class IconDemoWidget extends StatelessWidget {
   }
 }
 
+///Icon
 Widget newIcon(IconData icon) {
   return new Directionality(
       textDirection: TextDirection.ltr, child: new Icon(icon));
 }
 
+///文字
 Widget newText(String text) {
   return new Text(
     text ?? "text",
